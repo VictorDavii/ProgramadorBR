92f61a33b2e69d830ae20f20d3b52b71a57e58b0
VICTOR DAVI <113daviiicamach@gmail.com>
2023-09-28 17:30:09 -0300

    event DOM

---
+++ JS-intermediario/modificando-elementos/code.js
@@ -0,0 +1,14 @@
+//InnerText 
+
+let pInnerText = document.getElementsByTagName('p')[0]
+pInnerText.innerText += 'ADD pelo InnerText'
+//---------------------------------------------------////
+//TextContent
+let pContent = document.getElementsByTagName('p')[1]
+pContent.textContent += 'ADD pelo textContent'
+//---------------------------------------------------////
+//InnerHTML 
+let pInnerHTML = document.getElementsByTagName('p')[2]
+pInnerHTML.innerHTML += '<span> paragrafo add pelo InnerHTML</span>'
+
+pInnerText.setAttribute('meuATT', 'victordavi') 
